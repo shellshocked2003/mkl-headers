2ab12f1ab24d3ab56c4aff34403b758a821c1821
Michael Gardner <shellshocked2003@gmail.com>
2019-08-21 12:24:53 -0700

    Updated conanfile.py for header-only package

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+import os
 
 class mklDynamic(ConanFile):
     name = "mkl-include"
@@ -9,33 +10,34 @@ class mklDynamic(ConanFile):
     license = "Intel Simplified Software License"   
     settings = "os", "compiler", "build_type", "arch"
     description = "Intel Math Kernel Library Include Files"
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    no_copy_source = True
     build_policy = "missing"
 
     # Custom attributes for Bincrafters recipe conventions
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake    
-    
-    def build(self):
+    # def _configure_cmake(self):
+    #     cmake = CMake(self)
+    #     cmake.configure(build_folder=self._build_subfolder)
+    #     return cmake    
+
+    def source(self):
+        source_url = ""
         if self.settings.os == "Windows":
-            url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/win-64/mkl-include-2019.4-245.tar.bz2")
+            source_url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/win-64/mkl-include-2019.4-245.tar.bz2")
         elif self.settings.os == "Macos":
-            url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/osx-64/mkl-include-2019.4-233.tar.bz2")
+            source_url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/osx-64/mkl-include-2019.4-233.tar.bz2")
         elif self.settings.os == "Linux":
-            url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/linux-64/mkl-include-2019.4-243.tar.bz2")
+            source_url = ("https://anaconda.org/anaconda/mkl-include/2019.4/download/linux-64/mkl-include-2019.4-243.tar.bz2")
         else:
             raise Exception("Binary does not exist for these settings")
-        tools.get(url, destination=self._source_subfolder)
+       
+        tools.get(source_url, destination=self._source_subfolder)
 
     def package(self):
-        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder + "/info")
-        self.copy("*", dst="include", src=self._source_subfolder + "/include")        
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder + "/info")        
+        self.copy(pattern="*", dst="include", src=include_folder)
 
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+    def package_id(self):
+        self.info.header_only()
